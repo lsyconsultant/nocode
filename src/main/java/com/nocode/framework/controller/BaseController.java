b98422a47b52aa8c5f2be828e6a0c114487d200f
이수용 <lsyconsultant@gmail.com>
2023-05-09 09:18:10 +0900

    과장님꺼 건드렷음

--- src/main/java/com/nocode/framework/controller/BaseController.java
+++ src/main/java/com/nocode/framework/controller/BaseController.java
@@ -13,13 +13,8 @@ public class BaseController {
     public @ResponseBody String createProject(HttpServletRequest req){
         System.out.println("프로젝트 ID : " + req.getParameter("projectId"));
         System.out.println("프로젝트 이름 : " + req.getParameter("projectNm"));
-//<<<<<<< HEAD
-//        
-//        // 테스트 
-//=======
-//
-//        //수용 로그 남김
-//>>>>>>> refs/remotes/origin/master
+ 
+        //ㅇㅇㅇㅇㄹㄴㅇㄹㄴㅇㄹ
         return "성공1234";
     }
 }
